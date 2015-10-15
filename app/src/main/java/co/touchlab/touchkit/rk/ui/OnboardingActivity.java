f677c80734024a13b3fee46fa5963beced3034d1
wdziemia <wdziemia@gmail.com>
2015-10-15 15:46:14 -0400

    - Adds UI to Onboarding Activity - Adds SignInActivity and Eligibility Activity

--- app/src/main/java/co/touchlab/touchkit/rk/ui/OnboardingActivity.java
+++ app/src/main/java/co/touchlab/touchkit/rk/ui/OnboardingActivity.java
@@ -1,10 +1,34 @@
 package co.touchlab.touchkit.rk.ui;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import co.touchlab.touchkit.rk.R;
 
 /**
  * Created by bradleymcdermott on 10/15/15.
  */
 public class OnboardingActivity extends AppCompatActivity
 {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        super.setContentView(R.layout.activity_onboarding);
+
+
+    }
+
+    public void onSignUpClicked(View view)
+    {
+        startActivity(new Intent(this, EligibilityActivity.class));
+    }
+
+    public void onSignInClicked(View view)
+    {
+        startActivity(new Intent(this, SignInActivity.class));
+    }
 }
---
+++ app/src/main/java/co/touchlab/touchkit/rk/ui/SignInActivity.java
@@ -0,0 +1,15 @@
+package co.touchlab.touchkit.rk.ui;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import co.touchlab.touchkit.rk.R;
+
+public class SignInActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+    }
+}
