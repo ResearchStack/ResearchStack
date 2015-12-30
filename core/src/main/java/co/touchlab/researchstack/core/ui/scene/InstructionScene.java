28530eda99a0c084a458a9145ac848521fc817ad
wdziemia <wdziemia@gmail.com>
2015-12-30 14:52:33 -0500

    Removes SceneImpl extension from InstructionScene

--- core/src/main/java/co/touchlab/researchstack/core/ui/scene/InstructionScene.java
+++ core/src/main/java/co/touchlab/researchstack/core/ui/scene/InstructionScene.java
@@ -2,9 +2,23 @@ package co.touchlab.researchstack.core.ui.scene;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-public class InstructionScene extends SceneImpl<Void>
+import com.jakewharton.rxbinding.view.RxView;
+
+import co.touchlab.researchstack.core.R;
+import co.touchlab.researchstack.core.result.StepResult;
+import co.touchlab.researchstack.core.step.Step;
+import co.touchlab.researchstack.core.ui.callbacks.SceneCallbacks;
+
+public class InstructionScene extends RelativeLayout implements Scene
 {
+    private SceneCallbacks  callbacks;
+    private Step step;
+
     public InstructionScene(Context context)
     {
         super(context);
@@ -12,14 +26,56 @@ public class InstructionScene extends SceneImpl<Void>
 
     public InstructionScene(Context context, AttributeSet attrs)
     {
-        super(context,
-                attrs);
+        super(context, attrs);
     }
 
     public InstructionScene(Context context, AttributeSet attrs, int defStyleAttr)
     {
-        super(context,
-                attrs,
-                defStyleAttr);
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public void initialize(Step step, StepResult result)
+    {
+        this.step = step;
+        initializeScene();
+    }
+
+    private void initializeScene()
+    {
+        LayoutInflater.from(getContext()).inflate(R.layout.scene_instruction, this, true);
+
+        // Set Title
+        TextView titleView = (TextView) findViewById(R.id.title);
+        titleView.setText(step.getTitle());
+
+        // Set Summary
+        TextView summaryView = (TextView) findViewById(R.id.summary);
+        summaryView.setText(step.getText());
+
+        // Set Next
+        TextView next = (TextView) findViewById(R.id.next);
+        RxView.clicks(next).subscribe(v -> {
+            callbacks.onSaveStep(SceneCallbacks.ACTION_NEXT, step, null);
+        });
+    }
+
+    @Override
+    public View getView()
+    {
+        return this;
+    }
+
+    @Override
+    public boolean isBackEventConsumed()
+    {
+        callbacks.onSaveStep(SceneCallbacks.ACTION_PREV, step, null);
+        return false;
+    }
+
+    @Override
+    public void setCallbacks(SceneCallbacks callbacks)
+    {
+        this.callbacks = callbacks;
     }
 }
