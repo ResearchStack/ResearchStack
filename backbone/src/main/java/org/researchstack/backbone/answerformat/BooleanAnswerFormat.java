0dfa500869b26dcfb615aa4ed7413831a721542b
Brad McDermott <bradmcdermott@gmail.com>
2016-03-11 15:05:19 -0500

    Fixes boolean answer format's question type and value type

--- backbone/src/main/java/org/researchstack/backbone/answerformat/BooleanAnswerFormat.java
+++ backbone/src/main/java/org/researchstack/backbone/answerformat/BooleanAnswerFormat.java
@@ -9,6 +9,12 @@ public class BooleanAnswerFormat extends ChoiceAnswerFormat
     //TODO Fetch strings from Resources
     public BooleanAnswerFormat()
     {
-        super(ChoiceAnswerStyle.SingleChoice, new Choice<>("Yes", 1), new Choice<>("No", 0));
+        super(ChoiceAnswerStyle.SingleChoice, new Choice<>("Yes", true), new Choice<>("No", false));
+    }
+
+    @Override
+    public QuestionType getQuestionType()
+    {
+        return Type.Boolean;
     }
 }
