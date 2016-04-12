6c3f1f3e0d8ac7e31a74432cbd84ca816220ac69
Brad McDermott <bradmcdermott@gmail.com>
2016-04-12 14:37:30 -0400

    Adds docs for all remaining steps in backbone

--- backbone/src/main/java/org/researchstack/backbone/step/ConsentSignatureStep.java
+++ backbone/src/main/java/org/researchstack/backbone/step/ConsentSignatureStep.java
@@ -1,4 +1,9 @@
 package org.researchstack.backbone.step;
+
+/**
+ * This class represents the final step in the consent process, collecting the signature from the
+ * study participant.
+ */
 public class ConsentSignatureStep extends Step
 {
     private String signatureDateFormat;
@@ -9,11 +14,29 @@ public class ConsentSignatureStep extends Step
         setOptional(false);
     }
 
+    /**
+     * Returns the date format string to be used when producing a date string for the PDF or consent
+     * review.
+     * <p>
+     * For example, "yyyy-MM-dd 'at' HH:mm". When the value of this property is `nil`, the current
+     * date and time for the current locale is used.
+     *
+     * @return the date format string
+     */
     public String getSignatureDateFormat()
     {
         return signatureDateFormat;
     }
 
+    /**
+     * Sets the date format string to be used when producing a date string for the PDF or consent
+     * review.
+     * <p>
+     * For example, "yyyy-MM-dd 'at' HH:mm". When the value of this property is `nil`, the current
+     * date and time for the current locale is used.
+     *
+     * @param signatureDateFormat a string representing the date format
+     */
     public void setSignatureDateFormat(String signatureDateFormat)
     {
         this.signatureDateFormat = signatureDateFormat;
