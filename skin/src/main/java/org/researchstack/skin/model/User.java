1ee9007d0c12fb854f5c27a7793db9a4dbd0d958
Dario Salvi <dario.salvi.work@gmail.com>
2016-10-18 15:04:00 +0100

    added full name to user

--- skin/src/main/java/org/researchstack/skin/model/User.java
+++ skin/src/main/java/org/researchstack/skin/model/User.java
@@ -7,21 +7,36 @@ import java.io.Serializable;
  */
 public class User implements Serializable
 {
+    private String fullname;
+
     private String name;
 
     private String email;
 
     private String birthDate;
 
-    public User()
-    {
+    public User() {
     }
 
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    /**
+     * Retrieves the username, as registered on the server
+     */
     public String getName()
     {
         return name;
     }
 
+    /**
+     * Sets the username, as registered on the server.
+     */
     public void setName(String name)
     {
         this.name = name;
