69ad554f8e5fbb41e446944a8de4281a0acecb78
MDP <mike@sdpdigital.com>
2017-02-17 13:41:39 -0500

    Created short walk back and forth task and unit tests

--- backbone/src/main/java/org/researchstack/backbone/task/factory/WalkingTaskFactory.java
+++ backbone/src/main/java/org/researchstack/backbone/task/factory/WalkingTaskFactory.java
@@ -17,6 +17,7 @@ import org.researchstack.backbone.utils.ResUtils;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import static org.researchstack.backbone.task.factory.TaskFactory.Constants.*;
 
@@ -33,6 +34,9 @@ public class WalkingTaskFactory {
 
     private static final float DEFAULT_STEP_DURATION_FALLBACK_FACTOR = 1.5f;
     private static final int DEFAULT_COUNTDOWN_DURATION = 5; // in seconds
+
+    private static final int IGNORE_NUMBER_OF_STEPS = Integer.MAX_VALUE;
+    private static final int SPEAK_WALK_DURATION_HALFWAY_THRESHOLD = 20; // in seconds
 
     public static final String ShortWalkOutboundStepIdentifier        = "walking.outbound";
     public static final String ShortWalkReturnStepIdentifier          = "walking.return";
@@ -180,10 +184,12 @@ public class WalkingTaskFactory {
 
                     FitnessStep step = new FitnessStep(ShortWalkRestStepIdentifier);
 
-                    String titleFormat = context.getString(R.string.rsb_walk_stand_instruction_format);
-                    String title = String.format(titleFormat, convertDurationToString(restDuration));
+                    String titleFormat = context.getString(R.string.rsb_WALK_STAND_INSTRUCTION_FORMAT);
+                    String title = String.format(titleFormat, convertDurationToString(context, restDuration));
                     step.setTitle(title);
-                    step.setSpokenInstruction(title);
+                    String voiceTitleFormat = context.getString(R.string.rsb_WALK_STAND_VOICE_INSTRUCTION_FORMAT);
+                    String voiceTitle = String.format(voiceTitleFormat, convertDurationToString(context, restDuration));
+                    step.setSpokenInstruction(voiceTitle);
                     step.setRecorderConfigurationList(recorderConfigList);
                     step.setShouldContinueOnFinish(true);
                     step.setShouldStartTimerAutomatically(true);
@@ -205,20 +211,161 @@ public class WalkingTaskFactory {
     }
 
     /**
+     * Returns a predefined task that consists of a short walk back and forth.
+     *
+     * In a short walk task, the participant is asked to walk a short distance, which may be indoors.
+     * Typical uses of the resulting data are to assess stride length, smoothness, sway, or other aspects
+     * of the participant's gait.
+     *
+     * The presentation of the back and forth walk task differs from the short walk in that the participant
+     * is asked to walk back and forth rather than walking in a straight line for a certain number of steps.
+     *
+     * The participant is then asked to turn in a full circle and then stand still.
+     *
+     * This task is intended to allow the participant to walk in a confined space where the participant
+     * does not have access to a long hallway to walk in a continuous straight line. Additionally, by asking
+     * the participant to turn in a full circle and then stand still, the activity can access balance and
+     * concentration.
+     *
+     * The data collected by this task can include accelerometer, device motion, and pedometer data.
+     *
+     * @param context                 can be app or activity, used for resources
+     * @param identifier              The task identifier to use for this task, appropriate to the study.
+     * @param intendedUseDescription  A localized string describing the intended use of the data
+     *                                collected. If the value of this parameter is `nil`, the default
+     *                                localized text is displayed.
+     * @param walkDuration            The duration of the walking period in seconds.
+     * @param restDuration            The duration of the rest period in seconds.
+     *                                When the value of this parameter is
+     *                                nonzero, the user is asked to stand still for the specified rest
+     *                                period after the turn sequence has been completed, and baseline
+     *                                data is collected.
+     * @param optionList              Options that affect the features of the predefined task.
+     *
+     * @return An active short walk task that can be presented with an `ActiveTaskActivity` object.
+     */
+    public static OrderedTask walkBackAndForthTask(
+            Context context,
+            String identifier,
+            String intendedUseDescription,
+            int walkDuration,
+            int restDuration,
+            List<TaskExcludeOption> optionList)
+    {
+        List<Step> stepList = new ArrayList<>();
+
+        // Obtain sensor frequency for Walking Task recorders
+        double sensorFreq = context.getResources().getInteger(R.integer.rsb_sensor_frequency_tremor_task);
+
+        if (!optionList.contains(TaskExcludeOption.INSTRUCTIONS)) {
+            {
+                String title = context.getString(R.string.rsb_WALK_TASK_TITLE);
+                InstructionStep step = new InstructionStep(Instruction0StepIdentifier, title, intendedUseDescription);
+                step.setMoreDetailText(context.getString(R.string.rsb_WALK_INTRO_TEXT));
+                stepList.add(step);
+            }
+
+            {
+                String title = context.getString(R.string.rsb_WALK_TASK_TITLE);
+                String text = context.getString(R.string.rsb_WALK_INTRO_2_TEXT_BACK_AND_FORTH_INSTRUCTION);
+                InstructionStep step = new InstructionStep(Instruction1StepIdentifier, title, text);
+                step.setMoreDetailText(context.getString(R.string.rsb_WALK_INTRO_2_DETAIL_BACK_AND_FORTH_INSTRUCTION));
+                step.setImage(ResUtils.PHONE_IN_POCKET);
+                stepList.add(step);
+            }
+        }
+
+        {
+            CountdownStep step = new CountdownStep(CountdownStepIdentifier);
+            step.setStepDuration(DEFAULT_COUNTDOWN_DURATION);
+            stepList.add(step);
+        }
+
+        {
+            {
+                List<RecorderConfig> recorderConfigList = new ArrayList<>();
+                if (!optionList.contains(TaskExcludeOption.PEDOMETER)) {
+                    recorderConfigList.add(new PedometerRecorderConfig(PedometerRecorderIdentifier));
+                }
+                if (!optionList.contains(TaskExcludeOption.ACCELEROMETER)) {
+                    recorderConfigList.add(new AccelerometerRecorderConfig(AccelerometerRecorderIdentifier, sensorFreq));
+                }
+                if (!optionList.contains(TaskExcludeOption.DEVICE_MOTION)) {
+                    recorderConfigList.add(new AccelerometerRecorderConfig(DeviceMotionRecorderIdentifier, sensorFreq));
+                }
+
+                {
+                    WalkingTaskStep step = new WalkingTaskStep(ShortWalkOutboundStepIdentifier);
+                    String titleFormat = context.getString(R.string.rsb_WALK_BACK_AND_FORTH_INSTRUCTION_FORMAT);
+                    String title = String.format(titleFormat, convertDurationToString(context, walkDuration));
+                    step.setTitle(title);
+                    step.setSpokenInstruction(title);
+                    step.setRecorderConfigurationList(recorderConfigList);
+                    step.setShouldContinueOnFinish(true);
+                    step.setShouldStartTimerAutomatically(true);
+                    step.setStepDuration(walkDuration);
+                    step.setNumberOfStepsPerLeg(IGNORE_NUMBER_OF_STEPS);
+                    step.setShouldSpeakRemainingTimeAtHalfway(walkDuration > SPEAK_WALK_DURATION_HALFWAY_THRESHOLD);
+                    step.setShouldVibrateOnStart(true);
+                    step.setShouldPlaySoundOnStart(true);
+                    stepList.add(step);
+                }
+            }
+
+            if (restDuration > 0) {
+                if (restDuration > 0) {
+                    List<RecorderConfig> recorderConfigList = new ArrayList<>();
+                    if (!optionList.contains(TaskExcludeOption.ACCELEROMETER)) {
+                        recorderConfigList.add(new AccelerometerRecorderConfig(AccelerometerRecorderIdentifier, sensorFreq));
+                    }
+                    if (!optionList.contains(TaskExcludeOption.DEVICE_MOTION)) {
+                        recorderConfigList.add(new AccelerometerRecorderConfig(DeviceMotionRecorderIdentifier, sensorFreq));
+                    }
+
+                    FitnessStep step = new FitnessStep(ShortWalkRestStepIdentifier);
+
+                    String titleFormat = context.getString(R.string.rsb_WALK_BACK_AND_FORTH_STAND_INSTRUCTION_FORMAT);
+                    String title = String.format(titleFormat, convertDurationToString(context, restDuration));
+                    step.setTitle(title);
+                    step.setSpokenInstruction(title);
+                    step.setRecorderConfigurationList(recorderConfigList);
+                    step.setShouldContinueOnFinish(true);
+                    step.setShouldStartTimerAutomatically(true);
+                    step.setStepDuration(restDuration);
+                    step.setShouldVibrateOnStart(true);
+                    step.setShouldPlaySoundOnStart(true);
+                    step.setShouldVibrateOnFinish(true);
+                    step.setShouldPlaySoundOnFinish(true);
+                    step.setFinishedSpokenInstruction(context.getString(R.string.rsb_WALK_BACK_AND_FORTH_FINISHED_VOICE));
+                    step.setShouldSpeakRemainingTimeAtHalfway(restDuration > SPEAK_WALK_DURATION_HALFWAY_THRESHOLD);
+                    stepList.add(step);
+                }
+            }
+        }
+
+        if (!optionList.contains(TaskExcludeOption.CONCLUSION)) {
+            stepList.add(TaskFactory.makeCompletionStep(context));
+        }
+
+        return new OrderedTask(identifier, stepList);
+    }
+
+    /**
      * In iOS, this method turns duration into "for X minutes, Y seconds" but in Android,
      * you can only localize a duration to be  "in X minutes, Y seconds", so use that instead
      * @param durationInSeconds the duration in seconds
      * @return a string formatted to "in X minutes, Y seconds" where x & y are from durationInSeconds
      */
-    private static String convertDurationToString(int durationInSeconds) {
-        try {
-            long now = System.currentTimeMillis();
-            long then = now + (durationInSeconds * DateUtils.SECOND_IN_MILLIS);
-            return DateUtils.getRelativeTimeSpanString(then, now, DateUtils.SECOND_IN_MILLIS).toString().toLowerCase();
-        } catch (RuntimeException e) {
-            // this is purely to catch the unit test runtime exception here
-            // DateUtils is not a mock-able static class
-            return "";
+    private static String convertDurationToString(Context context, int durationInSeconds) {
+        int minutes = durationInSeconds / 60;
+        int seconds = durationInSeconds - minutes * 60;
+        if (minutes > 0) {
+            return String.format(Locale.getDefault(), "%d %s, %d %s",
+                    minutes, context.getString(R.string.rsb_minutes).toLowerCase(),
+                    seconds, context.getString(R.string.rsb_time_seconds).toLowerCase());
+        } else {
+            return String.format(Locale.getDefault(), "%d %s",
+                    seconds, context.getString(R.string.rsb_time_seconds).toLowerCase());
         }
     }
 
--- backbone/src/test/java/org/researchstack/backbone/task/factory/WalkingTaskTests.java
+++ backbone/src/test/java/org/researchstack/backbone/task/factory/WalkingTaskTests.java
@@ -2,21 +2,17 @@ package org.researchstack.backbone.task.factory;
 
 import android.content.Context;
 import android.content.res.Resources;
-import android.text.format.DateUtils;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 import org.researchstack.backbone.R;
 import org.researchstack.backbone.step.Step;
-import org.researchstack.backbone.step.active.PedometerRecorder;
 import org.researchstack.backbone.step.active.PedometerRecorderConfig;
 import org.researchstack.backbone.step.active.RecorderConfig;
 import org.researchstack.backbone.step.active.WalkingTaskStep;
-import org.researchstack.backbone.task.NavigableOrderedTask;
 import org.researchstack.backbone.task.OrderedTask;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
@@ -26,7 +22,6 @@ import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertTrue;
 import static org.researchstack.backbone.task.factory.TaskFactory.Constants.*;
-import static org.researchstack.backbone.task.factory.TremorTaskFactory.tremorTask;
 import static org.researchstack.backbone.task.factory.WalkingTaskFactory.*;
 
 /**
@@ -37,7 +32,6 @@ public class WalkingTaskTests {
 
     private Context mockContext;
     private Resources mockResources;
-    private DateUtils mockDateUtils;
 
     @Before
     public void setUp() throws Exception {
@@ -55,10 +49,20 @@ public class WalkingTaskTests {
         Mockito.when(mockContext.getString(R.string.rsb_WALK_RETURN_INSTRUCTION_FORMAT)).thenReturn("Walk up to %1$d steps in a straight line.");
 
         Mockito.when(mockContext.getString(R.string.rsb_WALK_STAND_VOICE_INSTRUCTION_FORMAT)).thenReturn("Now stand still for %1$s.");
-        Mockito.when(mockContext.getString(R.string.rsb_walk_stand_instruction_format)).thenReturn("Stand still for %1$s.");
+        Mockito.when(mockContext.getString(R.string.rsb_WALK_STAND_INSTRUCTION_FORMAT)).thenReturn("Stand still for %1$s.");
+        Mockito.when(mockContext.getString(R.string.rsb_WALK_STAND_VOICE_INSTRUCTION_FORMAT)).thenReturn("Stand still for %1$s.");
 
         Mockito.when(mockContext.getString(R.string.rsb_TASK_COMPLETE_TITLE)).thenReturn("");
         Mockito.when(mockContext.getString(R.string.rsb_TASK_COMPLETE_TEXT)).thenReturn("");
+
+        Mockito.when(mockContext.getString(R.string.rsb_minutes)).thenReturn("minutes");
+        Mockito.when(mockContext.getString(R.string.rsb_time_seconds)).thenReturn("seconds");
+
+        Mockito.when(mockContext.getString(R.string.rsb_WALK_BACK_AND_FORTH_FINISHED_VOICE)).thenReturn("");
+        Mockito.when(mockContext.getString(R.string.rsb_WALK_BACK_AND_FORTH_STAND_INSTRUCTION_FORMAT)).thenReturn("Turn in a full circle and then stand still for %1$s.");
+        Mockito.when(mockContext.getString(R.string.rsb_WALK_BACK_AND_FORTH_INSTRUCTION_FORMAT)).thenReturn("Walk back and forth in a straight line for %1$s. Walk as you would normally.");
+        Mockito.when(mockContext.getString(R.string.rsb_WALK_INTRO_2_TEXT_BACK_AND_FORTH_INSTRUCTION)).thenReturn("");
+        Mockito.when(mockContext.getString(R.string.rsb_WALK_INTRO_2_DETAIL_BACK_AND_FORTH_INSTRUCTION)).thenReturn("");
 
         Mockito.when(mockResources.getInteger(R.integer.rsb_sensor_frequency_default)).thenReturn(100);
     }
@@ -158,6 +162,109 @@ public class WalkingTaskTests {
                 i++;
             }
         } while (step != null);
+    }
+
+    @Test
+    public void testWalkBackAndForthTask() {
+        OrderedTask task = WalkingTaskFactory.walkBackAndForthTask(
+                mockContext, "walkingtaskid", "intendedUseDescription",
+                50, 10, Arrays.asList(new TaskExcludeOption[] {}));
+
+        List<String> stepIds = getWalkBackAndForthStepIds();
+        Step step = null;
+        int i = 0;
+        do {
+            step = task.getStepAfterStep(step, null);
+            if (step != null) {
+
+                if (step.getIdentifier().equals(ShortWalkOutboundStepIdentifier)) {
+                    WalkingTaskStep walkingStep = (WalkingTaskStep)step;
+                    boolean hasPedometer = false;
+                    for (RecorderConfig config : walkingStep.getRecorderConfigurationList()) {
+                        if (config instanceof PedometerRecorderConfig) {
+                            hasPedometer = true;
+                        }
+                    }
+                    assertTrue(hasPedometer);
+                }
+
+                assertEquals(step.getIdentifier(), stepIds.get(i));
+                i++;
+            }
+        } while (step != null);
+    }
+
+    @Test
+    public void testWalkBackAndForthTaskNoPedometer() {
+        OrderedTask task = WalkingTaskFactory.walkBackAndForthTask(
+                mockContext, "walkingtaskid", "intendedUseDescription",
+                50, 10, Collections.singletonList(TaskExcludeOption.PEDOMETER));
+
+        List<String> stepIds = getWalkBackAndForthStepIds();
+        Step step = null;
+        int i = 0;
+        do {
+            step = task.getStepAfterStep(step, null);
+            if (step != null) {
+                if (step.getIdentifier().equals(ShortWalkOutboundStepIdentifier)) {
+                    WalkingTaskStep walkingStep = (WalkingTaskStep)step;
+                    for (RecorderConfig config : walkingStep.getRecorderConfigurationList()) {
+                        assertFalse(config instanceof PedometerRecorderConfig);
+                    }
+                }
+                assertEquals(step.getIdentifier(), stepIds.get(i));
+                i++;
+            }
+        } while (step != null);
+    }
+
+    @Test
+    public void testWalkBackAndForthTaskNoInstructions() {
+        OrderedTask task = WalkingTaskFactory.walkBackAndForthTask(
+                mockContext, "walkingtaskid", "intendedUseDescription",
+                50, 10, Collections.singletonList(TaskExcludeOption.INSTRUCTIONS));
+
+        List<String> stepIds = getWalkBackAndForthStepIds();
+        stepIds.remove(Instruction0StepIdentifier);
+        stepIds.remove(Instruction1StepIdentifier);
+        Step step = null;
+        int i = 0;
+        do {
+            step = task.getStepAfterStep(step, null);
+            if (step != null) {
+                assertEquals(step.getIdentifier(), stepIds.get(i));
+                i++;
+            }
+        } while (step != null);
+    }
+
+    @Test
+    public void testWalkBackAndForthTaskNoConclusion() {
+        OrderedTask task = WalkingTaskFactory.walkBackAndForthTask(
+                mockContext, "walkingtaskid", "intendedUseDescription",
+                50, 10, Collections.singletonList(TaskExcludeOption.CONCLUSION));
+
+        List<String> stepIds = getWalkBackAndForthStepIds();
+        stepIds.remove(ConclusionStepIdentifier);
+        Step step = null;
+        int i = 0;
+        do {
+            step = task.getStepAfterStep(step, null);
+            if (step != null) {
+                assertEquals(step.getIdentifier(), stepIds.get(i));
+                i++;
+            }
+        } while (step != null);
+    }
+
+    private List<String> getWalkBackAndForthStepIds() {
+        return new LinkedList<>(Arrays.asList(
+                Instruction0StepIdentifier,
+                Instruction1StepIdentifier,
+                CountdownStepIdentifier,
+                ShortWalkOutboundStepIdentifier,
+                ShortWalkRestStepIdentifier,
+                ConclusionStepIdentifier));
     }
 
     private List<String> getShortWalkStepIds() {
