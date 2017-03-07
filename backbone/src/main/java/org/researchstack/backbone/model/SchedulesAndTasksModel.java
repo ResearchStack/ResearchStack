68dd6a4c454128286b1f799def5eff4ba1e0ac32
MDP <mike@sdpdigital.com>
2017-03-07 11:53:18 -0500

    started work on active task tests

--- backbone/src/main/java/org/researchstack/backbone/model/SchedulesAndTasksModel.java
+++ backbone/src/main/java/org/researchstack/backbone/model/SchedulesAndTasksModel.java
@@ -4,8 +4,12 @@ import com.google.gson.annotations.SerializedName;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
 
 public class SchedulesAndTasksModel {
+
+    public static final String TASK_TYPE_ACTIVITY = "Activity";
+
     public List<ScheduleModel> schedules;
 
     public static class ScheduleModel {
@@ -17,12 +21,22 @@ public class SchedulesAndTasksModel {
     }
 
     public static class TaskScheduleModel {
+
         public String taskTitle;
+
+        @SerializedName("taskIdentifier")
         public String taskID;
+
         public String taskFileName;
         public String taskClassName;
         public boolean taskIsOptional;
         public String taskType;
+        public String intendedUseDescription;
+
+        /**
+         * A map of optional parameters that should be used when creating and running the task
+         */
+        public Map<String, Object> taskOptions;
 
         @SerializedName("taskCompletionTimeString")
         public String taskCompletionTime;
