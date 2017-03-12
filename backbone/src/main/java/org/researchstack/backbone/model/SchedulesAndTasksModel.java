8585c5654fd1d0e3c7880f88d491de2dac25fefe
MDP <mike@sdpdigital.com>
2017-03-12 14:30:22 -0400

    reverted changes

--- backbone/src/main/java/org/researchstack/backbone/model/SchedulesAndTasksModel.java
+++ backbone/src/main/java/org/researchstack/backbone/model/SchedulesAndTasksModel.java
@@ -1,14 +1,9 @@
 package org.researchstack.backbone.model;
 
-import com.google.gson.annotations.SerializedName;
-
 import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
 public class SchedulesAndTasksModel {
-
-    public static final String TASK_TYPE_ACTIVITY = "Activity";
 
     public List<ScheduleModel> schedules;
 
@@ -21,24 +16,13 @@ public class SchedulesAndTasksModel {
     }
 
     public static class TaskScheduleModel {
-
         public String taskTitle;
-
-        @SerializedName("taskIdentifier")
         public String taskID;
-
         public String taskFileName;
         public String taskClassName;
         public boolean taskIsOptional;
         public String taskType;
         public String intendedUseDescription;
-
-        /**
-         * A map of optional parameters that should be used when creating and running the task
-         */
-        public Map<String, Object> taskOptions;
-
-        @SerializedName("taskCompletionTimeString")
         public String taskCompletionTime;
     }
 }
