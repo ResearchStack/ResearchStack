4bb3e6359a3864ba3a6732511d69c18548542289
MDP <mike@sdpdigital.com>
2017-03-17 21:25:45 -0400

    Fixed Serializable interface as an object

--- backbone/src/main/java/org/researchstack/backbone/model/survey/BaseSurveyItem.java
+++ backbone/src/main/java/org/researchstack/backbone/model/survey/BaseSurveyItem.java
@@ -1,12 +1,10 @@
 package org.researchstack.backbone.model.survey;
 
-import java.io.Serializable;
-
 /**
  * Created by TheMDP on 1/2/17.
  */
 
-public class BaseSurveyItem extends SurveyItem<Serializable> {
+public class BaseSurveyItem extends SurveyItem {
     /* Default constructor needed for serilization/deserialization of object */
     BaseSurveyItem() {
         super();
