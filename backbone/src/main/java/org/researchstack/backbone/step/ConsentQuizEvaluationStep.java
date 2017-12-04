83297fe42c4939942385ecf7536de801b7b69fba
Joshua Liu <liujoshua@users.noreply.github.com>
2017-12-04 13:56:52 -0800

    Merge skin into backbone

---
+++ backbone/src/main/java/org/researchstack/backbone/step/ConsentQuizEvaluationStep.java
@@ -0,0 +1,56 @@
+package org.researchstack.backbone.step;
+
+import org.researchstack.backbone.R;
+import org.researchstack.backbone.model.ConsentQuizModel;
+import org.researchstack.backbone.ui.layout.ConsentQuizEvaluationStepLayout;
+
+@Deprecated // use NavigationFormStep or NavigationSubtaskStep instead
+public class ConsentQuizEvaluationStep extends Step {
+    private ConsentQuizModel quizModel;
+
+    private int attempt;
+    private int incorrect;
+
+    public ConsentQuizEvaluationStep(String identifier, ConsentQuizModel quizModel) {
+        super(identifier);
+        this.quizModel = quizModel;
+    }
+
+    @Override
+    public int getStepTitle() {
+        return R.string.rsb_quiz_evaluation;
+    }
+
+    @Override
+    public Class getStepLayoutClass() {
+        return ConsentQuizEvaluationStepLayout.class;
+    }
+
+    public int getAttempt() {
+        return attempt;
+    }
+
+    public void setAttempt(int attempt) {
+        this.attempt = attempt;
+    }
+
+    public void setIncorrectCount(int incorrect) {
+        this.incorrect = incorrect;
+    }
+
+    public int getIncorrect() {
+        return incorrect;
+    }
+
+    public ConsentQuizModel getQuizModel() {
+        return quizModel;
+    }
+
+    public boolean isOverMaxAttempts() {
+        return attempt >= 1;
+    }
+
+    public boolean isQuizPassed() {
+        return incorrect <= quizModel.getAllowedFailures();
+    }
+}
--- skin/src/test/java/org/researchstack/skin/DataResponseTest.java
+++ skin/src/test/java/org/researchstack/skin/DataResponseTest.java
@@ -1,4 +1,4 @@
-package org.researchstack.skin;
+package org.researchstack.backbone;
 
 import org.junit.Before;
 import org.junit.Test;
