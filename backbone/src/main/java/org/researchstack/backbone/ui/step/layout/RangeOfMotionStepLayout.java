5a2ef97f6c8225897dcd073447e238206756d968
David Evans <d.w.evans@bham.ac.uk>
2019-01-18 15:08:44 +0000

    Adding RangeOfMotionStepLayout.java

---
+++ backbone/src/main/java/org/researchstack/backbone/ui/step/layout/RangeOfMotionStepLayout.java
@@ -0,0 +1,202 @@
+package org.researchstack.backbone.ui.step.layout;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.AttributeSet;
+import android.view.View;
+
+import org.researchstack.backbone.result.StepResult;
+import org.researchstack.backbone.step.Step;
+import org.researchstack.backbone.step.active.recorder.AudioRecorder;
+import org.researchstack.backbone.step.active.recorder.PedometerRecorder;
+import org.researchstack.backbone.step.active.RangeOfMotionStep;
+
+/**
+ * Created by TheMDP on 2/16/17.
+ *
+ * The RangeOfMotionStepLayout is basically the same as the ActiveStepLayout, except that it
+ * limits the duration of the step based on the user's number of steps taken so far
+ *
+ * It also shows an indefinite progress dialog, since we don't know if it will end based on
+ * the stepDuration or the numberOfStepsPerLeg
+ */
+
+public class RangeOfMotionStepLayout extends ActiveStepLayout {
+
+    private RangeOfMotionStep rangeOfMotionStep;
+    private BroadcastReceiver pedometerReceiver;
+
+    public RangeOfMotionStepLayout(Context context) {
+        super(context);
+    }
+
+    public RangeOfMotionStepLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RangeOfMotionStepLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public RangeOfMotionStepLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    public void initialize(Step step, StepResult result) {
+        super.initialize(step, result);
+
+        timerTextview.setVisibility(View.GONE);
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    protected void validateStep(Step step) {
+        if (!(step instanceof RangeOfMotionStep)) {
+            throw new IllegalStateException("RangeOfMotionStepLayout must have a RangeOfMotionStep");
+        }
+        rangeOfMotionStep = (RangeOfMotionStep) step;
+        super.validateStep(step);
+    }
+
+    @Override
+    protected void registerRecorderBroadcastReceivers(Context appContext) {
+        super.registerRecorderBroadcastReceivers(appContext);
+        pedometerReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                if (intent == null || intent.getAction() == null) {
+                    return;
+                }
+                if (DeviceMotionRecorder.BROADCAST_ROTATION_VECTOR_UPDATE_ACTION.equals(intent.getAction())) {
+                    DeviceMotionRecorder.DeviceMotionUpdateHolder dataHolder =
+                            DeviceMotionRecorder.getDeviceMotionUpdateHolder(intent);
+                    if (dataHolder != null) {
+                        if (RangeOfMotionStep.getNumberOfStepsPerLeg() > 0 &&
+                                (dataHolder.getStepCount() >= RangeOfMotionStep.getNumberOfStepsPerLeg()))
+                        {
+                            // TODO: mdephillips 1/13/18
+                            // we may want to move this functionality to the PedometerRecorder
+                            // and having that signal to RecorderService to stop,
+                            // since this StepLayout may be create/destroyed and miss this broadcast
+                            RangeOfMotionStepLayout.super.stop();
+                        }
+                    }
+                }
+            }
+        };
+        IntentFilter intentFilter = new IntentFilter(AudioRecorder.BROADCAST_SAMPLE_ACTION);
+        LocalBroadcastManager.getInstance(appContext)
+                .registerReceiver(pedometerReceiver, intentFilter);
+    }
+
+    @Override
+    protected void unregisterRecorderBroadcastReceivers() {
+        super.unregisterRecorderBroadcastReceivers();
+        Context appContext = getContext().getApplicationContext();
+        LocalBroadcastManager.getInstance(appContext).unregisterReceiver(pedometerReceiver);
+    }
+}
+
+
+@implementation ORKRangeOfMotionStepViewController
+
+        - (void)viewDidLoad {
+        [super viewDidLoad];
+        _contentView = [ORKRangeOfMotionContentView new];
+        _contentView.translatesAutoresizingMaskIntoConstraints = NO;
+        self.activeStepView.activeCustomView = _contentView;
+        _gestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(handleTap:)];
+        [self.activeStepView addGestureRecognizer:_gestureRecognizer];
+        }
+        //This function records the angle of the device when the screen is tapped
+        - (void)handleTap:(UIGestureRecognizer *)sender {
+        [self calculateAndSetAngles];
+        [self finish];
+        }
+
+        - (void)calculateAndSetAngles {
+        _startAngle = ([self getDeviceAngleInDegreesFromAttitude:_referenceAttitude]);
+
+        //This function calculates maximum and minimum angles recorded by the device
+        if (_newAngle > _maxAngle) {
+        _maxAngle = _newAngle;
+        }
+        if (_minAngle == 0.0 || _newAngle < _minAngle) {
+        _minAngle = _newAngle;
+        }
+        }
+
+        #pragma mark - ORKDeviceMotionRecorderDelegate
+
+        - (void)deviceMotionRecorderDidUpdateWithMotion:(CMDeviceMotion *)motion {
+        if (!_referenceAttitude) {
+        _referenceAttitude = motion.attitude;
+        }
+        CMAttitude *currentAttitude = [motion.attitude copy];
+
+        [currentAttitude multiplyByInverseOfAttitude:_referenceAttitude];
+
+        double angle = [self getDeviceAngleInDegreesFromAttitude:currentAttitude];
+
+        //This function shifts the range of angles reported by the device from +/-180 degrees to -90 to +270 degrees, which should be sufficient to cover all ahievable knee and shoulder ranges of motion
+        BOOL shiftAngleRange = angle > 90 && angle <= 180;
+        if (shiftAngleRange) {
+        _newAngle = fabs(angle) - 360;
+        } else {
+        _newAngle = angle;
+        }
+
+        [self calculateAndSetAngles];
+        }
+
+/*
+ When the device is in Portrait mode, we need to get the attitude's pitch
+ to determine the device's angle. attitude.pitch doesn't return all
+ orientations, so we use the attitude's quaternion to calculate the
+ angle.
+ */
+        - (double)getDeviceAngleInDegreesFromAttitude:(CMAttitude *)attitude {
+        if (!_orientation) {
+        _orientation = [UIApplication sharedApplication].statusBarOrientation;
+        }
+        double angle;
+        if (UIInterfaceOrientationIsLandscape(_orientation)) {
+        double x = attitude.quaternion.x;
+        double w = attitude.quaternion.w;
+        double y = attitude.quaternion.y;
+        double z = attitude.quaternion.z;
+        angle = radiansToDegrees(allOrientationsForRoll(x, w, y, z));
+        } else {
+        double x = attitude.quaternion.x;
+        double w = attitude.quaternion.w;
+        double y = attitude.quaternion.y;
+        double z = attitude.quaternion.z;
+        angle = radiansToDegrees(allOrientationsForPitch(x, w, y, z));
+        }
+        return angle;
+        }
+
+
+        #pragma mark - ORKActiveTaskViewController
+
+        - (ORKResult *)result {
+        ORKStepResult *stepResult = [super result];
+
+        ORKRangeOfMotionResult *result = [[ORKRangeOfMotionResult alloc] initWithIdentifier:self.step.identifier];
+
+        result.start = 90.0 - _startAngle;
+        result.finish = result.start - _newAngle;
+        //Because the task uses pitch in the direction opposite to the original CoreMotion device axes (i.e. right hand rule), maximum and minimum angles are reported the 'wrong' way around for the knee and shoulder tasks
+        result.minimum = result.start - _maxAngle;
+        result.maximum = result.start - _minAngle;
+        result.range = fabs(result.maximum - result.minimum);
+
+        stepResult.results = [self.addedResults arrayByAddingObject:result] ? : @[result];
+
+        return stepResult;
+        }
+
