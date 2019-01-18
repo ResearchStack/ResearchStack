09a0a145bb7c227eedd066dcc5202c7260a769f4
David Evans <d.w.evans@bham.ac.uk>
2019-01-18 15:12:32 +0000

    Update RangeOfMotionStep.java

--- backbone/src/main/java/org/researchstack/backbone/step/active/RangeOfMotionStep.java
+++ backbone/src/main/java/org/researchstack/backbone/step/active/RangeOfMotionStep.java
@@ -11,7 +11,7 @@ import java.util.Map;
 import java.util.UUID;
 
 /**
- * Created by David Evans. Copyright (c) 2019.
+ * Created by David Evans, 2019.
  */
 
 public class RangeOfMotionStep extends ActiveStep {
