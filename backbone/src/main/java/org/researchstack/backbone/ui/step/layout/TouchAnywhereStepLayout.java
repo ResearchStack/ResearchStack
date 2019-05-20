f1299b25be9f2ac69664065f76bfbc1542f260d4
David Evans <dwe@backpainclinic.co.uk>
2019-05-20 13:32:46 +0100

    Updated TouchAnywhereStepLayout

--- backbone/src/main/java/org/researchstack/backbone/ui/step/layout/TouchAnywhereStepLayout.java
+++ backbone/src/main/java/org/researchstack/backbone/ui/step/layout/TouchAnywhereStepLayout.java
@@ -3,33 +3,54 @@ package org.researchstack.backbone.ui.step.layout;
 import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
-import android.text.format.DateUtils;
 import android.util.AttributeSet;
-import android.view.Gravity;
 import android.view.View;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 
 import org.researchstack.backbone.R;
 import org.researchstack.backbone.result.StepResult;
 import org.researchstack.backbone.step.Step;
-import org.researchstack.backbone.step.active.TouchAnywhereStep;
+import org.researchstack.backbone.ui.callbacks.StepCallbacks;
+
 
 /**
- * Created by David Evans, 2019.
+ * Created by David Evans, Laurence Hurst, 2019.
  *
- * The TouchAnywhereStepLayout displays instruction text and sets an 'onclicklistener' for the main layout*/
+ * The TouchAnywhereStepLayout displays instruction text and sets an 'onclicklistener' for the main layout
+ *
+ * */
 
 public class TouchAnywhereStepLayout extends ActiveStepLayout {
 
     private RelativeLayout layout;
-    
+
+    public TouchAnywhereStepLayout(Context context) {
+        super(context);
+    }
+
+    public TouchAnywhereStepLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TouchAnywhereStepLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(21)
+    public TouchAnywhereStepLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void initialize(Step step, StepResult result) {
+        super.initialize(step, result);
+        setupTouchAnywhereViews();
+    }
+
+    private void setupTouchAnywhereViews() {
+        //super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
         
         layout = (RelativeLayout)findViewById(R.id.rsb_active_step_layout_touch_anywhere);
         layout.setOnClickListener(new View.OnClickListener() {
