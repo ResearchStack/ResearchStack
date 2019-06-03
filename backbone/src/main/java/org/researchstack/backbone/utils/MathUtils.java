4ae4b70500a586548a16be3355504c6fd4d68025
David Evans <dwe@backpainclinic.co.uk>
2019-06-03 09:38:34 +0100

    Added MathUtils file
    The range of motion tasks require some mathematical methods that are not available on Math.java. These include quaternion algebra, conversion from quaternion attitude to Euler angles, and calculation of maximum and minimum from a series of data.

---
+++ backbone/src/main/java/org/researchstack/backbone/utils/MathUtils.java
@@ -0,0 +1,104 @@
+package org.researchstack.backbone.utils;
+
+import java.lang.Math;
+
+public class MathUtils {
+
+
+    /**
+     * Method to obtain minimum from range of real numbers
+     **/
+
+    public double getMinimum(double data) {
+
+        double min = 0;
+
+        if (data < min) {
+            min = data;
+        }
+        return min;
+    }
+
+
+    /**
+     * Method to obtain maximum from range of real numbers
+     **/
+
+    public double getMaximum(double data) {
+
+        double max = 0;
+
+        if (data > max) {
+            max = data;
+        }
+        return max;
+    }
+
+
+     /**
+     * Method to multiply two quaternions (non-commutative)
+     **/
+
+    // for formula, see http://mathworld.wolfram.com/Quaternion.html
+    public float[] multiplyQuaternions(float[] q1, float[] q2) {
+
+        float[] productQuaternion = new float[4];
+
+        productQuaternion[0] = (q1[0] * q2[0]) - (q1[1] * q2[1]) - (q1[2] * q2[2]) - (q1[3] * q2[3]);
+        productQuaternion[1] = (q1[0] * q2[1]) + (q1[1] * q2[0]) + (q1[2] * q2[3]) - (q1[3] * q2[2]);
+        productQuaternion[2] = (q1[0] * q2[2]) - (q1[1] * q2[3]) + (q1[2] * q2[0]) + (q1[3] * q2[1]);
+        productQuaternion[3] = (q1[0] * q2[3]) + (q1[1] * q2[2]) - (q1[2] * q2[1]) + (q1[3] * q2[0]);
+
+        return productQuaternion;
+    }
+
+
+    /**
+     * Method to calculate the inverse (complex conjugate) of a quaternion
+     **/
+
+    // for formula, see http://mathworld.wolfram.com/Quaternion.html
+    public float[] calculateInverseOfQuaternion(float[] originalQuaternion) {
+
+        float[] inverseQuaternion = new float[4];
+
+        inverseQuaternion[0] = originalQuaternion[0];
+        inverseQuaternion[1] = (-1 * originalQuaternion[1]);
+        inverseQuaternion[2] = (-1 * originalQuaternion[2]);
+        inverseQuaternion[3] = (-1 * originalQuaternion[3]);
+
+        return inverseQuaternion;
+    }
+
+
+    /**
+     * Methods to calculate Euler angles from device attitude quaternions
+     **/
+
+    public double allOrientationsForPitch(double w, double x, double y, double z) {
+
+        double angle_in_rads;
+
+        angle_in_rads = (Math.atan2(2.0 * (x * w + y * z), 1.0 - 2.0 * (x * x + z * z)));
+
+        return angle_in_rads;
+    }
+
+    public double allOrientationsForRoll(double w, double x, double y, double z) {
+
+        double angle_in_rads;
+
+        angle_in_rads = (Math.atan2(2.0 * (y * w - x * z), 1.0 - 2.0 * (y * y + z * z)));
+
+        return angle_in_rads;
+    }
+
+    public double allOrientationsForYaw(double w, double x, double y, double z) {
+
+        double angle_in_rads;
+
+        angle_in_rads = (Math.asin(2.0 * (x * y - w * z)));
+
+        return angle_in_rads;
+    }
+}
