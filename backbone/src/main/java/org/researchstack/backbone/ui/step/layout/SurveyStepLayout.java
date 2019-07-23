90808d52e00d8af49911eedcaa1ad0de5af15407
Martin Marconcini <marconcini@gmail.com>
2019-07-23 19:31:30 +0200

    Optimize imports.

--- backbone/src/main/java/org/researchstack/backbone/ui/step/layout/SurveyStepLayout.java
+++ backbone/src/main/java/org/researchstack/backbone/ui/step/layout/SurveyStepLayout.java
@@ -9,7 +9,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.StringRes;
 import android.text.Html;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
