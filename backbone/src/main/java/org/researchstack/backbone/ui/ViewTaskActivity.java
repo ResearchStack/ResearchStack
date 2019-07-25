b2c80b5e3c8dfb9845e3a982ef5b1ced5a364a23
Soulemane Diaoune <soulemane@medable.com>
2019-07-25 09:22:36 -0400

    Add Flag_secure to UI Activities

--- backbone/src/main/java/org/researchstack/backbone/ui/ViewTaskActivity.java
+++ backbone/src/main/java/org/researchstack/backbone/ui/ViewTaskActivity.java
@@ -269,6 +269,7 @@ public class ViewTaskActivity extends PinCodeActivity implements StepCallbacks, 
     protected void onPause() {
         hideKeyboard();
         super.onPause();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE,WindowManager.LayoutParams.FLAG_SECURE);
 
         task.onViewChange(Task.ViewChangeType.ActivityPause, this, currentStep);
     }
@@ -276,6 +277,8 @@ public class ViewTaskActivity extends PinCodeActivity implements StepCallbacks, 
     @Override
     protected void onResume() {
         super.onResume();
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_SECURE);
+
         task.onViewChange(Task.ViewChangeType.ActivityResume, this, currentStep);
     }
 
