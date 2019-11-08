4a19f7ce348965071c5fb0350b6906360b6b6017
Norberto App <norbertoapp@gmail.com>
2019-11-08 18:23:09 -0300

    PAT-1294: fixing koing module to support Android X.

--- backbone/src/main/java/org/researchstack/backbone/ui/task/TaskModule.kt
+++ backbone/src/main/java/org/researchstack/backbone/ui/task/TaskModule.kt
@@ -1,10 +1,9 @@
 package org.researchstack.backbone.ui.task
 
 import android.content.Intent
-import org.koin.android.viewmodel.dsl.viewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val taskActivityModule = module {
-
     viewModel { (intent : Intent) -> TaskViewModel(get(), intent) }
 }
