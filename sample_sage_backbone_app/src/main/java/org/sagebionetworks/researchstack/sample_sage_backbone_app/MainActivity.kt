87cdd72f6cb01e8789159c821b0e88ac4483748f
Joshua Liu <liujoshua@users.noreply.github.com>
2019-11-13 06:55:11 -0800

    Create Sage Backbone interop modules

---
+++ sample_sage_backbone_app/src/main/java/org/sagebionetworks/researchstack/sample_sage_backbone_app/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.sagebionetworks.researchstack.sample_sage_backbone_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
