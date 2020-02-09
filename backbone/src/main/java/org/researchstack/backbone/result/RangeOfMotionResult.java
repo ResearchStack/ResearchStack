e9acb8d65b7fb3a47939488b2f11f84c3ae15826
David Evans <dwe@backpainclinic.co.uk>
2020-02-09 10:16:32 +0000

    Add initial device orientation to ROM results

--- backbone/src/main/java/org/researchstack/backbone/result/RangeOfMotionResult.java
+++ backbone/src/main/java/org/researchstack/backbone/result/RangeOfMotionResult.java
@@ -7,25 +7,35 @@ package org.researchstack.backbone.result;
 public class RangeOfMotionResult extends Result {
 
     /**
-     The angle (degrees) from the device reference position at the start position.
+     The orientation of the device at the start position:
+     ORIENTATION_UNSPECIFIED = -1
+     ORIENTATION_LANDSCAPE = 0
+     ORIENTATION_PORTRAIT = 1
+     ORIENTATION_REVERSE_LANDSCAPE = 2
+     ORIENTATION_REVERSE_PORTRAIT = 3
+     */
+    private int orientation;
+
+    /**
+    The angle (degrees) from the device reference position at the start position.
      */
     private double start;
 
     /**
-     The angle (degrees) from the device reference position when the task finishes recording.
+    The angle (degrees) from the device reference position when the task finishes recording.
      */
     private double finish;
 
     /**
-     The angle (degrees) from the device reference position at the minimum angle (e.g. when the knee is most bent, such as at the end of the task).
+    The angle (degrees) from the device reference position at the minimum angle (e.g. when the knee is most bent, such as at the end of the task).
      */
     private double minimum;
-
+    
     /**
-     The angle (degrees) from the device reference position at the maximum angle (e.g. when the knee is most extended during the task).
+    The angle (degrees) from the device reference position at the maximum angle (e.g. when the knee is most extended during the task).
      */
     private double maximum;
-
+    
     /**
      The angle (degrees) passed through from the minimum angle to the maximum angle (e.g. from when the knee is most flexed to when it is most extended).
      */
@@ -39,6 +49,14 @@ public class RangeOfMotionResult extends Result {
 
     public RangeOfMotionResult(String identifier) {
         super(identifier);
+    }
+
+    public int getOrientation() {
+        return orientation;
+    }
+
+    public void setOrientation(int orientation) {
+        this.orientation = orientation;
     }
 
     public double getStart() {
@@ -64,7 +82,7 @@ public class RangeOfMotionResult extends Result {
     public double getMaximum() {
         return maximum;
     }
-
+    
     public void setMaximum(double maximum) {
         this.maximum = maximum;
     }
@@ -74,5 +92,4 @@ public class RangeOfMotionResult extends Result {
     }
 
     public void setRange(double range) { this.range = range; }
-
 }
