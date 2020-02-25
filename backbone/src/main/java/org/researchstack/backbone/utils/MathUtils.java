8def65d613596497e1bb7754afd36db3055f9b3c
David Evans <dwe@backpainclinic.co.uk>
2020-02-25 19:13:34 +0000

    Update MathUtils.java

--- backbone/src/main/java/org/researchstack/backbone/utils/MathUtils.java
+++ backbone/src/main/java/org/researchstack/backbone/utils/MathUtils.java
@@ -7,7 +7,7 @@ public class MathUtils {
      /**
      * Method to multiply two quaternions (non-commutative).
      * For original formula, see http://mathworld.wolfram.com/Quaternion.html
-     **/
+     */
     public static float[] multiplyQuaternions(float[] q1, float[] q2) {
         float[] productQuaternion = new float[4];
         productQuaternion[0] = (q1[0] * q2[0]) - (q1[1] * q2[1]) - (q1[2] * q2[2]) - (q1[3] * q2[3]);
@@ -21,7 +21,7 @@ public class MathUtils {
     /**
      * Method to calculate the inverse (complex conjugate) of a quaternion.
      * For original formula, see http://mathworld.wolfram.com/Quaternion.html
-     **/
+     */
     public static float[] calculateInverseOfQuaternion(float[] originalQuaternion) {
         float[] inverseQuaternion = new float[4];
         inverseQuaternion[0] = originalQuaternion[0];
@@ -34,7 +34,7 @@ public class MathUtils {
 
     /**
      * Methods to calculate Tait-Bryan/Euler angles from device attitude quaternions
-     **/
+     */
     public static double allOrientationsForPitch(double w, double x, double y, double z) {
         double angle_in_rads;
         angle_in_rads = (Math.atan2(2.0 * (x * w + y * z), 1.0 - 2.0 * (x * x + z * z)));
@@ -47,7 +47,7 @@ public class MathUtils {
         return angle_in_rads;
     }
 
-    public static double allOrientationsForYaw(double w, double x, double y, double z) {
+    public static double allOrientationsForAzimuth(double w, double x, double y, double z) {
         double angle_in_rads;
         angle_in_rads = (Math.asin(2.0 * (x * y - w * z)));
         return angle_in_rads;
