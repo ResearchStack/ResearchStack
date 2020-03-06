f144674c0ed8af404c9093777e66f1aa09679a0f
David Evans <dwe@backpainclinic.co.uk>
2020-03-06 13:09:48 +0000

    Update TouchAnywhereStep.java

--- backbone/src/main/java/org/researchstack/backbone/step/active/TouchAnywhereStep.java
+++ backbone/src/main/java/org/researchstack/backbone/step/active/TouchAnywhereStep.java
@@ -1,41 +1,46 @@
+package org.researchstack.backbone.step.active;
 
-package com.spineapp;
+import org.researchstack.backbone.ui.step.layout.TouchAnywhereStepLayout;
 
-import org.researchstack.backbone.step.active.ActiveStep;
+/**
+ * Created by David Evans, Laurence Hurst, 2019.
+ *
+ * The `TouchAnywhereStep` class represents a step that enables the user to see instructions
+ * and then begin the following step by touching anywhere on the screen.
+ *
+ */
 
-public class TouchAnywhereStep extends ActiveStep
-{
+public class TouchAnywhereStep extends ActiveStep {
 
-  /*Default constructor needed for serilization/deserialization of object*/
+    /* Default constructor needed for serilization/deserialization of object */
+    public TouchAnywhereStep() {
+        super();
+    }
 
-  //public TouchAnywhereStep() { //TODO: not sure why this method creates an error
-  //super();
-  //}
+    public TouchAnywhereStep(String identifier) {
+        super(identifier);
+        commonInit();
+    }
 
-  public TouchAnywhereStep(String identifier) {
-    super(identifier);
-    commonInit();
-  }
+    public TouchAnywhereStep(String identifier, String title, String detailText) {
+        super(identifier, title, detailText);
+        commonInit();
+    }
 
-  public TouchAnywhereStep(String identifier, String title, String detailText) {
-    super(identifier, title, detailText);
-    commonInit();
-  }
+    private void commonInit() {
+        setOptional(false);
+        setShouldShowDefaultTimer(false);
+        setShouldVibrateOnStart(true);
+        setShouldPlaySoundOnStart(true);
+        setShouldVibrateOnFinish(true);
+        setShouldPlaySoundOnFinish(true);
+        setShouldStartTimerAutomatically(true);
+        setShouldContinueOnFinish(true);
+        setEstimateTimeInMsToSpeakEndInstruction(0); // do not wait to proceed
+    }
 
-  private void commonInit() {
-    setOptional(false);
-    setShouldVibrateOnStart(true);
-    setShouldPlaySoundOnStart(true);
-    setShouldVibrateOnFinish(true);
-    setShouldPlaySoundOnFinish(true);
-    setShouldStartTimerAutomatically(true);
-    setShouldShowDefaultTimer(false);
-    setShouldContinueOnFinish(true);
-    setEstimateTimeInMsToSpeakEndInstruction(0); // do not wait to proceed
-  }
-
-  @Override
-  public Class getStepLayoutClass() {
-    return TouchAnywhereStepLayout.class;
-  }
+    @Override
+    public Class getStepLayoutClass() {
+        return TouchAnywhereStepLayout.class;
+    }
 }
