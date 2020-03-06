61871bec13496c1ed5d487cd1dd19aee3c550a51
David Evans <dwe@backpainclinic.co.uk>
2020-03-06 13:00:40 +0000

    Update MathUtils.java

--- backbone/src/main/java/org/researchstack/backbone/utils/MathUtils.java
+++ backbone/src/main/java/org/researchstack/backbone/utils/MathUtils.java
@@ -4,40 +4,9 @@ import java.lang.Math;
 
 public class MathUtils {
 
-    /**
-     * Method to calculate standard deviation from an array.
-     **/
-    public static double calculateSDFromArray(double [] numArray)
-    {
-        double sum = 0, standardDeviation = 0;
-        int length = numArray.length;
-        for(double num : numArray) {
-            sum += num;
-        }
-        double mean = sum/length;
-        for(double num: numArray) {
-            standardDeviation += Math.pow(num - mean, 2);
-        }
-        return Math.sqrt(standardDeviation/length);
-    }
-
-    /**
-     * Method to evaluate whether an integer is even.
-     */
-    public static boolean isEven(int n) {
-        return n % 2 == 0;
-    }
-
-    /**
-     * Method to evaluate whether an integer is odd.
-     */
-    public static boolean isOdd(int n) {
-        return Math.abs(n % 2) == 1;
-    }
-
-    /**
-     * Method to multiply two quaternions (non-commutative). For formula, see
-     * http://mathworld.wolfram.com/Quaternion.html
+     /**
+     * Method to multiply two quaternions (non-commutative).
+     * For original formula, see http://mathworld.wolfram.com/Quaternion.html
      */
     public static float[] multiplyQuaternions(float[] q1, float[] q2) {
         float[] productQuaternion = new float[4];
@@ -48,23 +17,24 @@ public class MathUtils {
         return productQuaternion;
     }
 
+
     /**
-     * Method to calculate the inverse (complex conjugate) of a quaternion. For formula, see
-     * http://mathworld.wolfram.com/Quaternion.html
+     * Method to calculate the inverse (complex conjugate) of a quaternion.
+     * For original formula, see http://mathworld.wolfram.com/Quaternion.html
      */
     public static float[] calculateInverseOfQuaternion(float[] originalQuaternion) {
         float[] inverseQuaternion = new float[4];
         inverseQuaternion[0] = originalQuaternion[0];
-        inverseQuaternion[1] = (-1.0f * originalQuaternion[1]);
-        inverseQuaternion[2] = (-1.0f * originalQuaternion[2]);
-        inverseQuaternion[3] = (-1.0f * originalQuaternion[3]);
+        inverseQuaternion[1] = (-1 * originalQuaternion[1]);
+        inverseQuaternion[2] = (-1 * originalQuaternion[2]);
+        inverseQuaternion[3] = (-1 * originalQuaternion[3]);
         return inverseQuaternion;
     }
+
 
     /**
      * Methods to calculate Tait-Bryan/Euler angles from device attitude quaternions
      */
-
     public static double allOrientationsForPitch(double w, double x, double y, double z) {
         double angle_in_rads;
         angle_in_rads = (Math.atan2(2.0 * (x * w + y * z), 1.0 - 2.0 * (x * x + z * z)));
@@ -76,54 +46,6 @@ public class MathUtils {
         angle_in_rads = (Math.atan2(2.0 * (x * z - w * y), 1.0 - 2.0 * (x * x + y * y)));
         return angle_in_rads;
     }
-
-    // TODO: testing roll alternatives
-
-    // same as 1
-    public static double allOrientationsForRoll11(double w, double x, double y, double z) {
-        double angle_in_rads;
-        angle_in_rads = (Math.atan2(2.0 * (x * z - w * y), 1.0 - 2.0 * (x * x + y * y)));
-        return angle_in_rads;
-    }
-
-    // nearly correct (opposite sign for start and fin)
-    public static double allOrientationsForRoll6(double w, double x, double y, double z) {
-        double angle_in_rads;
-        angle_in_rads = (Math.atan2(2.0 * (x * w - y * z), 1.0 - 2.0 * (w * w + z * z)));
-        return angle_in_rads;
-    }
-
-    // nearly correct (opposite sign for start and fin)
-    public static double allOrientationsForRoll7(double w, double x, double y, double z) {
-        double angle_in_rads;
-        angle_in_rads = (Math.atan2(2.0 * (x * z - w * y), 1.0 - 2.0 * (w * w + z * z)));
-        return angle_in_rads;
-    }
-
-    public static double allOrientationsForRoll14(double w, double x, double y, double z) {
-        double angle_in_rads;
-        angle_in_rads = (Math.atan2(2.0 * (x * y - z * w), 1.0 - 2.0 * (x * x + y * y)));
-        return angle_in_rads;
-    }
-
-    public static double allOrientationsForRoll15(double w, double x, double y, double z) {
-        double angle_in_rads;
-        angle_in_rads = (Math.atan2(2.0 * (x * y + z * w), 1.0 - 2.0 * (x * x + y * y)));
-        return angle_in_rads;
-    }
-
-    public static double allOrientationsForRoll16(double w, double x, double y, double z) {
-        double angle_in_rads;
-        angle_in_rads = (Math.atan2(2.0 * (x * y - z * w), 1.0 - 2.0 * (y * y + z * z)));
-        return angle_in_rads;
-    }
-
-    public static double allOrientationsForRoll17(double w, double x, double y, double z) {
-        double angle_in_rads;
-        angle_in_rads = (Math.atan2(2.0 * (x * y + z * w), 1.0 - 2.0 * (x * x + z * z)));
-        return angle_in_rads;
-    }
-
 
     public static double allOrientationsForAzimuth(double w, double x, double y, double z) {
         double angle_in_rads;
